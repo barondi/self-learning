ce5714705f9eb09de936404d465bf784068547e2
dijs1 <dijs1@lenovo.com>
2016-11-22 13:38:13 +0800

    update delayed loading

--- utils.js
+++ utils.js
@@ -15,5 +15,27 @@ var utils={
     },
     getElementStyle:function(ele,attr){
         return window.getComputedStyle ? getComputedStyle(ele,null)[attr] : ele.currentStyle[attr];
+    },
+    //计算网页上某个DOM元素距离浏览器的偏移量
+    getOffset:function(ele){
+        var l=ele.offsetLeft;
+        var t=ele.offsetTop;
+        var p=ele.offsetParent;
+        while(l){
+            if(!p||p==document.body){
+                break;
+            }
+            if(window.navigator.userAgent.indexOf('MSIE 8.0')>-1){
+                //如果是IE8
+                l+= p.offsetLeft;
+                t+= p.offsetTop;
+            }else{
+                //在标准浏览器下，需要加上边框的宽
+                l+= p.offsetLeft+ p.clientLeft;
+                t+= p.offsetTop+ p.clientTop;
+            }
+            p= p.offsetParent;
+        }
+        return {left:l,top:t}
     }
 };
