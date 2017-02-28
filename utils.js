a09a397834c0d704830cecee3dffb0d971850284
dijs1 <dijs1@lenovo.com>
2017-02-28 18:11:28 +0800

    update webworker

--- utils.js
+++ utils.js
@@ -11,6 +11,103 @@ var utils={
         return arr;
     },
 
+    /*
+    * getIndex:获取指定dom元素的索引值
+    * */
+    getIndex:function(ele){
+        var index=0;
+        var p=ele.previousSibling;
+        while(p){
+            if(p.nodeType===1){
+                index++;
+            }
+            p= p.previousSibling;
+        }
+        return index;
+    },
+
+    /*
+     * getSiblings:获取指定dom元素的所有元素兄弟节点
+     * */
+    getSiblings:function(ele){
+        var nodes=ele.parentNode.childNodes;
+        var temp=[];
+        for(var i=0;i<nodes.length;i++){
+            if(nodes[i]!=this && nodes[i].nodeType===1){
+                temp.push(nodes[i]);
+            }
+        }
+        return temp;
+    },
+
+    /*
+    * prevSiblings:获取指定dom元素的所有哥哥元素节点
+    * */
+    prevSiblings:function(ele){//找哥哥元素思想是遇到自己就返回
+        var nodes=ele.parentNode.childNodes;
+        var temp=[];
+        for(var i=0;i<nodes.length;i++){
+            if(nodes[i]==ele){
+                break;
+            }
+            if(nodes[i].nodeType===1){
+                temp.push(nodes[i]);
+            }
+        }
+        return temp;
+    },
+
+    /*
+     * nextSiblings:获取指定dom元素的所有哥哥元素节点
+     * */
+    nextSiblings:function(ele){
+        var nodes=ele.parentNode.childNodes;
+        var temp=[];
+        for(var i=nodes.length-1;i>=0;i--){
+            if(nodes[i]==ele){
+                break;
+            }
+            if(nodes[i].nodeType===1){
+                temp.unshift(nodes[i]);
+            }
+        }
+        return temp;
+    },
+
+    /*
+    * prev:获取指定dom元素相邻的唯一的哥哥
+    * */
+    prev:function(ele){
+        //var temp = utils.prevSiblings(ele);
+        //return temp[temp.length-1];
+        var p = ele.previousSibling;
+        while(p){
+            if(p.nodeType==1){
+                return p;
+            }
+            p= p.previousSibling;
+        }
+        return null;//表示此方法应该有返回值,但是得不到,则应该返回null;如果不写return null,找不到结果,则返回undefined
+
+    },
+
+    /*
+    * next:获取指定dom元素相邻的唯一的弟弟
+    * */
+    next:function(ele){
+        var temp=utils.nextSiblings(ele);
+        return temp[0];
+    },
+
+    /*
+    * closest:获取指定dom元素相邻的哥哥和弟弟
+    * */
+    closest:function(ele){
+        var temp1=utils.prev(ele);
+        var temp2=utils.next(ele);
+        return [temp1,temp2];
+    },
+
     toJSON:function(str){
         return "JSON" in window ? JSON.parse(str) : eval("("+str+")");
     },
@@ -40,6 +137,28 @@ var utils={
             p= p.offsetParent;
         }
         return {left:l,top:t}
+    },
+
+    /*
+    * getElementsByCN:根据类名获取对应的元素
+    * 如果是单个类名,则先通过eles=(context || document).getElementsByTagNmae('*')获取元素,然后传递给形参;
+    * 如果是多个类名,则先通过下方strTrim方法对自己定义类名字符串格式化,然后通过split(' ')分割成数组,在循环中依次调用getElementsByCN.
+    * 详细示例参见js_third day_02中1.选项卡性能优化.html
+    * */
+    getElementsByCN:function(strClass,eles){
+        var reg=new RegExp("(^| )"+strClass+"( |$)");
+        var temp=[];
+        for(var i=0;i<eles.length;i++){
+            if(reg.test(eles[i].className)){
+                temp.push(eles[i]);
+            }
+        }
+        return temp;
+    },
+    strTrim:function(str){
+        str=str.replace(/(^\s+)|(\s+$)/g,'');
+        str=str.replace(/\s{2,}/g,' ');
+        return str;
     },
 
     /*
